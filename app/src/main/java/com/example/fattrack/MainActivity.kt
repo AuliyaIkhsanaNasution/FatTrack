87ca0cb483756ab4077f261c29218c5870cf6a53
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-11-12 14:35:52 +0700

    add nav buttom

--- app/src/main/java/com/example/fattrack/MainActivity.kt
+++ app/src/main/java/com/example/fattrack/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.fattrack
 
+import android.app.FragmentManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.fattrack.databinding.ActivityMainBinding
+import com.example.fattrack.view.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        navview = findViewById(R.id.nav_view)
+
+        navview.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.navigation_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
