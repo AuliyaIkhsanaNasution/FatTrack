251d9f2fbd07c96ff115b47f2014580b7e65fb18
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-11-13 11:42:11 +0700

    perbaiki bug nav bottom

--- app/src/main/java/com/example/fattrack/MainActivity.kt
+++ app/src/main/java/com/example/fattrack/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : AppCompatActivity() {
         // Apply window insets to ensure the system bars are not overlapping
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
