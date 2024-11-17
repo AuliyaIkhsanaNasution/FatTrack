6ad97cab3a81a62c4f5aa0e3bcb9d9339e0c84e5
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-11-17 16:41:57 +0700

    add : register activity

---
+++ app/src/main/java/com/example/fattrack/view/register/RegisterActivity.kt
@@ -0,0 +1,32 @@
+package com.example.fattrack.view.register
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fattrack.R
+import com.example.fattrack.view.login.LoginActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    // Metode untuk menangani klik pada TextView
+    fun onRegisterClick(view: View) {
+        // Contoh: Navigasi ke activity registrasi
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
