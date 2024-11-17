aea1635b2a2fc945e052dfa812b1ee5d5ed985ef
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-11-17 16:38:58 +0700

    update :potrait login and landscape login

--- app/src/main/java/com/example/fattrack/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/fattrack/view/login/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.fattrack.view.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.fattrack.R
+import com.example.fattrack.view.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,12 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    // Metode untuk menangani klik pada TextView
+    fun onRegisterClick(view: View) {
+        // Contoh: Navigasi ke activity registrasi
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
