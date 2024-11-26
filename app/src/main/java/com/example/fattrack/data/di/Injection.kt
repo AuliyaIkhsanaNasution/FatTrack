08040211df32f6d2c3a2d7cba5efaea05267f639
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-11-26 12:30:20 +0700

    add : process injection

--- app/src/main/java/com/example/fattrack/data/di/Injection.kt
+++ app/src/main/java/com/example/fattrack/data/di/Injection.kt
@@ -1,4 +1,12 @@
 package com.example.fattrack.data.di
 
+import com.example.fattrack.data.repositories.ArticleRepository
+import com.example.fattrack.data.services.retrofit.ApiConfig
+
 object Injection {
+
+    fun provideArticlesRepository(): ArticleRepository {
+        val apiService = ApiConfig.getApiService()
+        return ArticleRepository.getInstance(apiService)
+    }
 }
