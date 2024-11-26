be1bdd102ebecf1aab023f8100b3aa4becab5f85
AuliyaIkhsanaNasution <auliyanasti06@gmail.com>
2024-11-26 11:43:19 +0700

    update : article repo

--- app/src/main/java/com/example/fattrack/data/repositories/ArticleRepository.kt
+++ app/src/main/java/com/example/fattrack/data/repositories/ArticleRepository.kt
@@ -1,4 +1,13 @@
 package com.example.fattrack.data.repositories
 
-class ArticleRepository {
+import com.example.fattrack.services.retrofit.ApiService
+
+class ArticleRepository (private val apiService: ApiService){
+
+    companion object {
+        fun getInstance (apiService: ApiService): ArticleRepository {
+            return ArticleRepository(apiService)
+        }
+    }
+
 }
