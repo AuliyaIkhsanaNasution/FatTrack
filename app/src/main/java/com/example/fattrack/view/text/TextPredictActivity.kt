43793ddb08aa9d275f7070904a1c671cfcb76d9d
MuhammadBobby <m.bobbyoktaviano@gmail.com>
2024-12-04 12:06:50 +0700

    Update: Search Food UI and Response API

--- app/src/main/java/com/example/fattrack/view/text/TextPredictActivity.kt
+++ app/src/main/java/com/example/fattrack/view/text/TextPredictActivity.kt
@@ -1,11 +1,24 @@
 package com.example.fattrack.view.text
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Spanned
+import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import cn.pedant.SweetAlert.SweetAlertDialog
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.fattrack.R
 import com.example.fattrack.data.ViewModelFactory
 import com.example.fattrack.data.data.NutritionData
@@ -14,6 +27,8 @@ import com.example.fattrack.data.viewmodel.PredictViewModel
 import com.example.fattrack.databinding.ActivityTextPredictBinding
 import com.example.fattrack.view.MyBottomSheetFragment
 import io.github.muddz.styleabletoast.StyleableToast
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TextPredictActivity : AppCompatActivity() {
 
@@ -28,16 +43,15 @@ class TextPredictActivity : AppCompatActivity() {
         binding = ActivityTextPredictBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        observeViewModel()
-        setupUI()
+        //container chat
+        val chatContainer: LinearLayout = binding.chatContainer
 
-        binding.back.setOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
-        }
+        observeViewModel(chatContainer)
+        setupUI(chatContainer)
     }
 
     @SuppressLint("SetTextI18n")
-    private fun observeViewModel() {
+    private fun observeViewModel(chatContainer: LinearLayout) {
         predictViewModel.searchResponse.observe(this) { response ->
             if (response != null) {
                 val foodData = response.data?.mapNotNull { foodItem ->
@@ -45,41 +59,165 @@ class TextPredictActivity : AppCompatActivity() {
                 }
 
                 if (!foodData.isNullOrEmpty()) {
-                    displayBottomSheet(foodData[0])
+                    //get data
+                    val imageUrl = "https://asset.kompas.com/crops/U6YxhTLF-vrjgM8PN3RYTHlIxfM=/84x60:882x592/1200x800/data/photo/2021/11/17/61949959e07d3.jpg"
+                    val desc = foodData[0].deskripsi
+                    val nameFood = foodData[0].nama
+                    val calorie = foodData[0].kalori
+                    val carb = foodData[0].karbohidrat
+                    val fat = foodData[0].lemak
+                    val protein = foodData[0].protein
+
+                    //message
+                    val message1 = """
+                        Tentu, yang kamu cari adalah <b>${nameFood?.let { capitalizeWords(it) }}</b>.
+                        <br> $desc
+                        """.trimIndent()
+                    val message2  = """
+                            <b>${nameFood?.let { capitalizeWords(it) }}</b> memiliki Nutrisi sebagai berikut:
+                            <br>- <b>Protein</b>: $protein g
+                            <br>- <b>Kalori</b>: $calorie kcal
+                            <br>- <b>Karbohidrat</b>: $carb g
+                            <br>- <b>Lemak</b>: $fat g
+                        """.trimIndent()
+
+                    //send response chat
+                    binding.chatbotStatus.text = "Online"
+                    addBubbleImage(chatContainer, imageUrl)
+                    lifecycleScope.launch {
+                        delay(300)
+                        binding.chatbotStatus.text = "Typing..."
+                        delay(800)
+                        binding.chatbotStatus.text = "Online"
+                        addBubbleChat(chatContainer, message1, false)
+                        delay(300)
+                        binding.chatbotStatus.text = "Typing..."
+                        delay(800)
+                        addBubbleChat(chatContainer, message2, false)
+                        binding.chatbotStatus.text = "Online"
+                    }
                 } else {
-                    showToast("No matching food found.")
+                    addBubbleChat(chatContainer, "Maaf, kami tidak dapat menemukan makanan yang sesuai.", false)
                 }
             } else {
-                showToast("Response is null.")
+                Log.d("TAG", "Response is null.")
             }
         }
 
+        //loading
         predictViewModel.isLoading.observe(this) { isLoading ->
-            binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
-            binding.btnPredict.isEnabled = !isLoading
+            binding.chatbotStatus.text = if (isLoading) "Typing..." else "Online"
         }
 
+        //error message
         predictViewModel.errorMessage.observe(this) { errorMessage ->
-            errorMessage?.let { showErrorDialog(it) }
+            addBubbleChat(chatContainer, "Maaf, kami gagal mencari data makanan yang anda inginkan", false)
         }
     }
 
-    private fun setupUI() {
-        binding.btnPredict.setOnClickListener {
-            val query = binding.edTextPredict.text.toString().trim()
-            if (query.isNotEmpty()) {
-                predictViewModel.searchFood(query)
+
+    //setup ui
+    @SuppressLint("SetTextI18n")
+    private fun setupUI(chatContainer: LinearLayout) {
+        binding.sendButton.setOnClickListener {
+            //get message
+            val message = binding.inputMessage.text.toString().trim()
+
+            if (message.isNotEmpty()) {
+                //send message
+                addBubbleChat(chatContainer, message, true)
+                //clear text
+                binding.inputMessage.text.clear()
+                //response with delay
+                binding.chatbotStatus.text = "Typing..."
+                lifecycleScope.launch {
+                    delay(1000)
+                    predictViewModel.searchFood(message)
+                }
             } else {
                 showToast("Please enter a food name.")
             }
         }
     }
 
-    private fun displayBottomSheet(foodData: NutritionData) {
-        val bottomSheetFragment = MyBottomSheetFragment.newInstance(foodData)
-        bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+
+    //setup bubble chat
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun addBubbleChat(container: LinearLayout, message: String, isUser: Boolean) {
+        // create bubble chat
+        val bubble = TextView(this).apply {
+            text = message.toHtmlSpanned()
+            textSize = 16f
+            setTextColor(if (isUser) android.graphics.Color.WHITE else android.graphics.Color.BLACK)
+            setPadding(24, 20, 24, 20)
+            background = getDrawable(if (isUser) R.drawable.chat_bubble_user else R.drawable.chat_bubble_other)
+            minWidth = 400
+            maxWidth = 800
+            maxLines = 10
+        }
+
+        // layout property
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        ).apply {
+            gravity = if (isUser) Gravity.END else Gravity.START
+            setMargins(8, 8, 8, 8)
+        }
+
+        // add bubble to container
+        bubble.layoutParams = params
+        container.addView(bubble)
+
+        // auto scroll down
+        val scrollView = container.parent as? ScrollView
+        scrollView?.post { scrollView.fullScroll(View.FOCUS_DOWN) }
     }
 
+
+    //bubble image
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun addBubbleImage(container: LinearLayout, imageUrl: String) {
+        Log.d("PredictImageTest", "image : $imageUrl")
+        // image view in Linear
+        val imageBubble = ImageView(this).apply {
+            background = getDrawable(R.drawable.chat_bubble_other)
+
+            // Set layout params untuk ImageView
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                //set padding
+                setPadding(12,12,12,12)
+                setMargins(8,8,8,8)
+                gravity = Gravity.START
+            }
+            // image set
+            scaleType = ImageView.ScaleType.CENTER_CROP
+            adjustViewBounds = true
+            maxWidth = 900
+        }
+
+
+        // glide image url
+        Glide.with(this)
+            .load(imageUrl)
+            .apply(RequestOptions().transform(RoundedCorners(100)))
+            .into(imageBubble)
+
+        // add to container
+        container.addView(imageBubble)
+        // auto scroll down
+        val scrollView = container.parent as? ScrollView
+        scrollView?.post {
+            scrollView.smoothScrollTo(0, container.height)
+        }
+    }
+
+
+
+    //Mapping
     private fun mapToParcelable(response: FoodDataItem?): NutritionData {
         return NutritionData(
             deskripsi = response?.deskripsi,
@@ -91,16 +229,31 @@ class TextPredictActivity : AppCompatActivity() {
         )
     }
 
+    //toast
     private fun showToast(message: String) {
         val toastCustom = StyleableToast.makeText(applicationContext, message, R.style.StyleableToast)
         toastCustom.show()
     }
 
-    private fun showErrorDialog(message: String?) {
-        SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
-            .setTitleText("Failed!")
-            .setContentText("$message. Please try again.")
-            .setConfirmText("OK")
-            .show()
+
+    //html parse
+    fun String.toHtmlSpanned(): Spanned {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            Html.fromHtml(this)
+        }
     }
+
+
+    //capitalize format
+    fun capitalizeWords(input: String): String {
+        return input.split(" ")
+            .joinToString(" ") { word ->
+                word.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase() else it.toString()
+                }
+            }
+    }
+
 }
